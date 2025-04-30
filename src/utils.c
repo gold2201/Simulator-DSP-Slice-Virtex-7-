a43d938a3e6157765e97cc473ace5df15bc8dea8
Lapilike <lapi.like@yandex.ru>
2025-04-30 23:09:39 +0300

    Fixes
    Signed-off-by: Lapilike <lapi.like@yandex.ru>

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,16 @@
+#include "dsp_simulator.h"
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+int64_t mask64_to_48(int64_t num) {
+    int64_t masked = num & RESULT_MASK;
+    if(num & (1ULL << 47)) {
+        masked = masked | (-CARRYOUT_MASK);
+    }
+    return masked;
+}
 
 void parse_key_value(const char *token, const char *key, char *dest, size_t dest_size) {
     if (strncmp(token, key, strlen(key)) == 0 && token[strlen(key)] == '=') {
